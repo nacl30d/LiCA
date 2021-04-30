6da840b1d6a8ffedac8beab56a728d806d05062f
d-salt <dev@d-salt.net>
2021-04-30 22:10:12 +0900

    ADD: prettier

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,13 @@ import { Router, Request, Response, NextFunction } from 'express';
 const indexRouter: Router = Router();
 
 /* GET home page. */
-indexRouter.get('/', (req: Request, res: Response, next: NextFunction): Response => {
-    return res
-        .status(200)
-        .json({
-            message: "Hello world!",
-        });
-});
+indexRouter.get(
+  '/',
+  (req: Request, res: Response, next: NextFunction): Response => {
+    return res.status(200).json({
+      message: 'Hello world!',
+    });
+  }
+);
 
 export default indexRouter;
